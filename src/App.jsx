2697f39410bbdb7c25673a76a5b11ceecf41ec8a
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-16 19:39:11 -0300

    :lipstick: feat: Adiciona gradiente de fundo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import styled from "styled-components"
+
+const FundoGradiente = styled.div`
+    background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+    min-height: 100vh;
+    width: 100%;
+`
+
 const App = () => {
 
     return (
-        <>
+        <FundoGradiente>
 
-        </>
+        </FundoGradiente>
     )
 }
 
