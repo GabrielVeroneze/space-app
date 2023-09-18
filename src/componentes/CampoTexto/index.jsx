f0a6f7df23bad9a84aea348b43b0d65aeb53a0b4
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-18 17:55:19 -0300

    :sparkles: feat: Cria componente CampoTexto

---
+++ src/componentes/CampoTexto/index.jsx
@@ -0,0 +1,38 @@
+import styled from "styled-components"
+import search from "./search.svg"
+
+const ContainerEstilizado = styled.div`
+    align-self: center;
+    display: flex;
+    position: relative;
+`
+
+const InputEstilizado = styled.input`
+    background-color: transparent;
+    border: 2px solid #C98CF1;
+    border-radius: 10px;
+    color: #D9D9D9;
+    font-size: 1.25rem;
+    height: 60px;
+    line-height: 1.25rem;
+    outline: none;
+    padding: 0.75rem 1rem;
+    width: 602px;
+`
+
+const IconeLupa = styled.img`
+    align-self: center;
+    position: absolute;
+    right: 18px;
+`
+
+const CampoTexto = () => {
+    return (
+        <ContainerEstilizado>
+            <InputEstilizado placeholder="O que você procura?" />
+            <IconeLupa src={search} alt="Ícone de Lupa" />
+        </ContainerEstilizado>
+    )
+}
+
+export default CampoTexto
