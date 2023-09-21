302709f4090a63fb4cfd06dc820e424fd97f7f34
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-21 16:49:46 -0300

    :sparkles: feat: Cria componente de Banner

---
+++ src/componentes/Banner/index.jsx
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+
+const FigureEstilizada = styled.figure`
+    align-items: center;
+    background: url(${props => props.$imagem}) center / cover no-repeat;
+    border-radius: 20px;
+    display: flex;
+    grid-area: banner;
+    height: 328px;
+    margin: 0;
+    padding: 0 4rem;
+`
+
+const TituloEstilizado = styled.h1`
+    color: #FFF;
+    font-size: 2.5rem;
+    font-weight: 400;
+    line-height: normal;
+    margin: 0;
+    min-width: 266px;
+    width: 29.5%;
+`
+
+const Banner = ({ imagemFundo, texto }) => {
+    return (
+        <FigureEstilizada $imagem={imagemFundo}>
+            <TituloEstilizado>
+                {texto}
+            </TituloEstilizado>
+        </FigureEstilizada>
+    )
+}
+
+export default Banner
