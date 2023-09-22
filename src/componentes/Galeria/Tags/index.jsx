34ec385bfae762f5bc218a908fa21adfe60c173d
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-22 18:09:15 -0300

    :sparkles: feat: Finaliza o componente de Tags

--- src/componentes/Galeria/Tags/index.jsx
+++ src/componentes/Galeria/Tags/index.jsx
@@ -1,6 +1,44 @@
+import styled from 'styled-components'
+import tags from './tags.json'
+
+const TagsEstilizada = styled.section`
+    align-items: center;
+    display: flex;
+    gap: 1.5rem;
+    margin-top: 1rem;
+`
+
+const TagTitulo = styled.h3`
+    color: #D9D9D9;
+    font-size: 1.5rem;
+    font-weight: 400;
+    line-height: normal;
+    margin: 0;
+`
+
+const BotaoEstilizado = styled.button`
+    color: #FFF;
+    border: 2px solid transparent;
+    border-radius: 10px;
+    background-color: rgba(217, 217, 217, 0.30);
+    cursor: pointer;
+    font-size: 1.5rem;
+    line-height: normal;
+    padding: 0.5rem;
+
+    &:hover {
+        border-color: #C98CF1;
+    }
+`
+
 const Tags = () => {
     return (
-        <div>Tags</div>
+        <TagsEstilizada>
+            <TagTitulo>Busque por tags:</TagTitulo>
+            {tags.map(tag => (
+                <BotaoEstilizado key={tag.id}>{tag.titulo}</BotaoEstilizado>
+            ))}
+        </TagsEstilizada>
     )
 }
 
