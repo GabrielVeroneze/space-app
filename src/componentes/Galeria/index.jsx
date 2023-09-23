b9025f9b23ac3e7c3d401508076018d667c59a0e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-22 23:33:44 -0300

    :sparkles: feat: Adiciona lista com imagens da galeria

--- src/componentes/Galeria/index.jsx
+++ src/componentes/Galeria/index.jsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import Tags from "./Tags"
 import Titulo from "../Titulo"
 import Populares from "./Populares"
+import Imagem from "./Imagem"
 
 const LayoutGaleria = styled.div`
     display: flex;
@@ -11,14 +12,16 @@ const GaleriaContainer = styled.section`
     flex-grow: 1;
 `
 
-const Galeria = () => {
+const Galeria = ({ fotos = [] }) => {
     return (
         <>
             <Tags />
             <LayoutGaleria>
                 <GaleriaContainer>
                     <Titulo>Navegue pela galeria</Titulo>
-
+                    <ul>
+                        {fotos.map(foto => <Imagem key={foto.id} {...foto} />)}
+                    </ul>
                 </GaleriaContainer>
                 <Populares />
             </LayoutGaleria>
