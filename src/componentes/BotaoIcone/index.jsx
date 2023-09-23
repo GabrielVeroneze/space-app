4c1c07dc7a3b5e96e6813136451ebeeea25393ca
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-23 16:20:45 -0300

    :sparkles: feat: Cria componente estilizado
    Cria componente estilizado para ícones clicaveis

---
+++ src/componentes/BotaoIcone/index.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const BotaoIcone = styled.button`
+    align-items: center;
+    display: flex;
+    background: none;
+    border: none;
+    cursor: pointer;
+    height: 24px;
+    justify-content: center;
+    padding: 0;
+    width: 24px;
+`
+
+export default BotaoIcone
