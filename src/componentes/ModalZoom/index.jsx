2d585406cb4c7acaf6413359f854908c501ab95e
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-25 23:07:42 -0300

    :sparkles: feat: Cria componente de Modal

--- src/componentes/ModalZoom/index.jsx
+++ src/componentes/ModalZoom/index.jsx
@@ -1,11 +1,33 @@
-const ModalZoom = () => {
+import styled from "styled-components";
+import Imagem from "../Galeria/Imagem";
+
+const Overlay = styled.div`
+    background: rgba(0, 0, 0, 0.7);
+    inset: 0;
+    position: fixed;
+`
+
+const ModalEstilizado = styled.dialog`
+    background: none;
+    border: none;
+    padding: 0;
+    position: fixed;
+    top: 294px;
+    width: 90vw;
+    margin: 0 auto;
+    height: 740px;
+`
+
+const ModalZoom = ({ foto }) => {
     return (
-        <dialog open>
-            <p>Greetings, one and all!</p>
-            <form method="dialog">
-                <button>OK</button>
-            </form>
-        </dialog>
+        <>
+            {foto && <>
+                <Overlay />
+                <ModalEstilizado open={foto}>
+                    <Imagem {...foto} expandida={true} />
+                </ModalEstilizado>    
+            </>}
+        </>
     )
 }
 
