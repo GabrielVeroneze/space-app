7b89782828a9d23f4789be8418ccf104a4dbee84
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2023-09-26 19:56:42 -0300

    :sparkles: feat: Finaliza seção de populares

--- src/componentes/Galeria/Populares/index.jsx
+++ src/componentes/Galeria/Populares/index.jsx
@@ -1,10 +1,47 @@
+import styled from "styled-components";
 import Titulo from "../../Titulo"
+import fotosPopulares from "./fotos-populares.json"
+
+const ColunaFotos = styled.aside`
+    display: flex;
+    flex-direction: column;
+    gap: 1.5rem;
+    margin: 1.5rem 0;
+`
+
+const ImagemEstilizada = styled.img`
+    border-radius: 20px;
+    max-width: 212px;
+`
+
+const Botao = styled.button`
+    background: none;
+    border: 2px solid #C98CF1;
+    border-radius: 10px;
+    color:#FFF;
+    cursor: pointer;
+    font-family: 'GandhiSans-Bold';
+    font-size: 1.25rem;
+    line-height: normal;
+    padding: 0.875rem; //
+    width: 100%;
+`
 
 const Populares = () => {
     return (
-        <div>
+        <section>
             <Titulo $alinhamento="center">Populares</Titulo>
-        </div>
+            <ColunaFotos>
+                {fotosPopulares.map(foto => (
+                    <ImagemEstilizada
+                        key={foto.id}
+                        src={foto.path}
+                        alt={foto.alt}
+                    />
+                ))}
+            </ColunaFotos>
+            <Botao>Ver mais</Botao>
+        </section>
     )
 }
 
